9c65a4429f2da6f8d43e70836d20990af64e7a10
daniel.roclawski <daniel.roclawski@gmail.com>
2022-06-06 19:52:29 +0200

    dodanie plików

---
+++ App1/BlankPage1.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class BlankPage1 : Page
+    {
+        public BlankPage1()
+        {
+            this.InitializeComponent();
+            kwota.Text = "";
+            ilosc500.Text = String.Empty;
+            ilosc200.Text = String.Empty;
+            ilosc100.Text = String.Empty;
+            ilosc50.Text = String.Empty;
+            pass.Password = "";
+        }
+        int reszta = 0, reszta1 = 0, reszta2 = 0, reszta3 = 0, iloscPieniedzy;
+
+        private async void zaloguj_Click(object sender, RoutedEventArgs e)
+        {
+            if(pass.Password == "admin")
+            {
+                kwadrat.Visibility = Visibility.Collapsed; 
+            }
+            else
+            {
+                string message3 = "Złe hasło!";
+                MessageDialog messageDialog = new MessageDialog(message3);
+                await messageDialog.ShowAsync();
+                
+            }
+        }
+
+        private async void Wyplac_Click(object sender, RoutedEventArgs e)
+        {
+            iloscPieniedzy = Convert.ToInt32(kwota.Text);
+            reszta = 0; reszta1 = 0; reszta2 = 0; reszta3 = 0;
+
+            if (iloscPieniedzy >= 50 && iloscPieniedzy %50 == 0)
+            {
+                
+                while (iloscPieniedzy != 0)
+                {
+                   
+                    if (iloscPieniedzy >= 500 && piecset.Value >= 1)
+                    {
+                        iloscPieniedzy -= 500;
+                        reszta++;
+                        piecset.Value --;
+                    }
+                    else if ((iloscPieniedzy <= 500 && iloscPieniedzy >= 200 && dwiescie.Value >= 1)
+                        || (iloscPieniedzy % 200 == 0 && dwiescie.Value == 0 && sto.Value >= 1)
+                        || (iloscPieniedzy % 200 == 50 && piecdziesiat.Value == 0 && sto.Value > 0))
+                    {
+                        iloscPieniedzy -= 200;
+                        reszta1++;
+                        dwiescie.Value--;
+                    }
+                    else if((iloscPieniedzy <=200 && iloscPieniedzy>=100 &&sto.Value>=1)
+                        ||(iloscPieniedzy %100 == 0 && dwiescie.Value == 0 && sto.Value >= 1 ) 
+                        || (iloscPieniedzy % 100 ==50 && piecdziesiat.Value == 0 && sto.Value> 0))
+                    {
+                        iloscPieniedzy -= 100;
+                        reszta2++;
+                        sto.Value --;
+                    }
+                    else if ((iloscPieniedzy <= 100 && iloscPieniedzy >= 50 && piecdziesiat.Value >= 1) 
+                        || (iloscPieniedzy % 50 == 0 && dwiescie.Value == 0 && sto.Value == 0 && piecdziesiat.Value >= 1) 
+                        || iloscPieniedzy % 50 == 0 && piecdziesiat.Value >= 1)
+                    {
+                        iloscPieniedzy -= 50;
+                        reszta3++;
+                        piecdziesiat.Value--;
+                    }
+                    else if ((dwiescie.Value == 0 && sto.Value == 0 && piecdziesiat.Value == 0)
+                        || (dwiescie.Value > 0 && sto.Value > 0 && piecdziesiat.Value == 0)
+                        || (dwiescie.Value > 0 && sto.Value == 0 && piecdziesiat.Value == 0 && iloscPieniedzy % 100 == 0)
+                        || (dwiescie.Value * 200 + sto.Value * 100 + piecdziesiat.Value * 50 < iloscPieniedzy)
+                        || (dwiescie.Value > 0 && piecdziesiat.Value > 0 && sto.Value > 100 && iloscPieniedzy % 50 == 0))
+                    {
+                        string message2 = "Za mało banknotów w bankomacie!";
+                        MessageDialog messageDialog = new MessageDialog(message2);
+                        await messageDialog.ShowAsync();
+                        break;
+
+
+                    }
+                }
+            }
+            else
+            {
+                string message3 = "Źle wpisano kwotę!";
+                MessageDialog messageDialog = new MessageDialog(message3);
+                await messageDialog.ShowAsync();
+            }
+            ilosc500.Text = Convert.ToString(reszta);
+            ilosc200.Text = Convert.ToString(reszta1);
+            ilosc100.Text = Convert.ToString(reszta2);
+            ilosc50.Text = Convert.ToString(reszta3);
+        }
+    }
+}
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace App1
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            pass.Password = "";
+            pass.MaxLength = 4;
+        }
+
+        private async void Zatwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            if (pass.Password == "1234")
+            {
+                this.Frame.Navigate(typeof(BlankPage1));
+            }
+            else
+            {
+                string message = "Zły PIN!";
+                MessageDialog msgdialog = new MessageDialog(message);
+                await msgdialog.ShowAsync();
+            }
+            if (pass.Password.Length > 4)
+            {
+                btn1.IsEnabled =true;
+                btn2.IsEnabled =true;
+                btn3.IsEnabled =true;
+                btn4.IsEnabled =true;
+                btn5.IsEnabled =true;
+                btn6.IsEnabled =true;
+                btn7.IsEnabled =true;
+                btn8.IsEnabled =true;
+                btn9.IsEnabled =true;
+                btn0.IsEnabled =true;
+            }
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "1";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "3";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "4";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "6";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "9";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            pass.Password += "0";
+        }
+
+       
+        private void pass_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            pass.MaxLength = 4;
+            if (pass.Password.Length >= 4)
+            {
+                btn1.IsEnabled = false;
+                btn2.IsEnabled = false;
+                btn3.IsEnabled = false;
+                btn4.IsEnabled = false;
+                btn5.IsEnabled = false;
+                btn6.IsEnabled = false;
+                btn7.IsEnabled = false;
+                btn8.IsEnabled = false;
+                btn9.IsEnabled = false;
+                btn0.IsEnabled = false;
+            }
+
+            if (pass.Password.Length < 4)
+            {
+                btn1.IsEnabled = true;
+                btn2.IsEnabled = true;
+                btn3.IsEnabled = true;
+                btn4.IsEnabled = true;
+                btn5.IsEnabled = true;
+                btn6.IsEnabled = true;
+                btn7.IsEnabled = true;
+                btn8.IsEnabled = true;
+                btn9.IsEnabled = true;
+                btn0.IsEnabled = true;
+
+            }
+
+            if (pass.Password.Length == 4)
+            {
+                ZATWIERDZ.IsEnabled = true;
+            }
+            else if (pass.Password.Length >= 1 && pass.Password.Length < 4)
+            {
+                ZATWIERDZ.IsEnabled = false;
+            }
+        }
+    }
+}
